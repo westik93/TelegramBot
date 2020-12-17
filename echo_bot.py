304bafc179ba5dc17e6af658c086468474fa2f88
Khongor Khodjigorov <westik08@mail.ru>
2020-12-18 01:56:01 +0300

    second commit

--- echo_bot.py
+++ echo_bot.py
@@ -2,6 +2,11 @@ import telebot
 
 bot = telebot.TeleBot("1487416357:AAEP0eg_P1tkqkHE8U-qOiZkdFmYJELoB24")
 
+@bot.message_handler(commands=['start'])
+def welcome(message):
+	sti = open('static/welcome.webp', 'rb')
+	bot.send_sticker(message.chat.id, sti)
+	
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
 	bot.send_message(message.chat.id, message.text)
